8067552c45e01da0592e20ada3dfcc4b9a2ec6a4
MarkKh <85799327+MarkKh@users.noreply.github.com>
2021-07-24 22:13:49 +0700

    Delete bk directory

--- bk/tt1.java
+++
@@ -1,9 +0,0 @@
-class tt1{
-  public static void main(String[] args) {
-    int sum = 100;
-      for(int i = sum; i>0; i--) {
-        if(i%5 == 0)
-        System.out.print(i + ",");
-      }
-  }
-}
